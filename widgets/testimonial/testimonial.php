61919affd362129f0faffdedd43874ad62dce31c
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2017-10-26 16:30:49 +0800

    Changed description

--- widgets/testimonial/testimonial.php
+++ widgets/testimonial/testimonial.php
@@ -12,7 +12,7 @@ class RA_Testimonial_Widget extends SiteOrigin_Widget {
             'rawb-testimonial',
             __( 'RA Testimonial', 'ra-widgets-bundle' ),
             array(
-                'description' => __( 'A simple image carousel widget', 'ra-widgets-bundle' ),
+                'description' => __( 'A simple testimonial widget', 'ra-widgets-bundle' ),
                 'help' => ''
             ),
             array(),
