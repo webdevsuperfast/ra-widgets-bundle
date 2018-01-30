99c693a6b8efae7a2682b4dc060e830fabc64ea7
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2018-01-30 16:33:54 +0800

    Removed unneeded line of codes, added thumbnail function

--- lib/misc.php
+++ lib/misc.php
@@ -1,28 +1,21 @@
 <?php
-// Custom Image Function
-function rawb_post_image() {
-	global $post;
-	$image = '';
-	$image_id = get_post_thumbnail_id( $post->ID );
-	$image = wp_get_attachment_image_src( $image_id, 'full' );
-	$image = $image[0];
-	if ( $image ) return $image;
-	return rawb_get_first_image();
-}
+// Get an array of registered images
+function rawb_thumb_sizes() {
+	global $_wp_additional_image_sizes;
+ 
+    $sizes = array(
+		'full' => __( 'Full', 'ra-widgets-bundle' )
+	);
 
-// Get the First Image Attachment Function
-function rawb_get_first_image() {
-	global $post, $posts;
-	$first_img = '';
-	ob_start();
-	ob_end_clean();
-	$output = preg_match_all( '/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches );
-	$first_img = "";
-	if ( isset( $matches[1][0] ) )
-		$first_img = $matches[1][0];
-	return $first_img;
-}
+	$get_intermediate_image_sizes = get_intermediate_image_sizes();
 
-function rawb_thumb($url, $width, $height=0, $align='') {
-    return rawb_image_resize($url, $width, $height, true, $align, false);
+	
+	
+	foreach( $get_intermediate_image_sizes as $_size ) {
+		if ( in_array( $_size, array( 'thumbnail', 'medium', 'large' ) ) ) {
+			$sizes[ $_size ] = ucwords( $_size ); // strtouppercase
+		}
+	}
+
+	return $sizes;
 }
